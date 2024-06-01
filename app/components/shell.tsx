bc058a787a1ad061e5a1ce8bf7c14880e86631a7
VHall1 <victorhallpsn@hotmail.com>
2024-06-01 19:36:15 +0100

    add theme switch thingy

--- app/components/shell.tsx
+++ app/components/shell.tsx
@@ -1,9 +1,45 @@
-import { Link } from "@remix-run/react";
-import { SquareKanbanIcon } from "lucide-react";
+import { Link, useFetcher, useRouteLoaderData } from "@remix-run/react";
+import { CheckIcon, SquareKanbanIcon } from "lucide-react";
 import { ReactNode } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { loader } from "~/root";
+import { Theme } from "~/utils/theme";
 
 export function Shell({ children }: { children: ReactNode }) {
+  const rootLoaderData = useRouteLoaderData<typeof loader>("root");
+  const userEmail = rootLoaderData?.userEmail;
+  const theme = rootLoaderData?.theme;
+
+  const logoutFetcher = useFetcher();
+  const logout = () => {
+    logoutFetcher.submit(null, {
+      method: "post",
+      action: "/action/logout",
+    });
+  };
+
+  const setThemeFetcher = useFetcher();
+  const setTheme = (nextTheme: Theme) => {
+    if (theme === nextTheme) return;
+    setThemeFetcher.submit(
+      { theme: nextTheme },
+      { method: "post", action: "/action/set-theme" }
+    );
+  };
+
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <header className="sticky top-0 z-50 w-full border-b border-border bg-background">
@@ -15,10 +51,63 @@ export function Shell({ children }: { children: ReactNode }) {
             </Link>
           </div>
           <div className="flex flex-1 justify-end">
-            <Avatar className="w-6 h-6">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar className="w-6 h-6">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel className="text-muted-foreground text-xs font-normal">
+                  Account
+                </DropdownMenuLabel>
+                <DropdownMenuGroup>
+                  <div className="flex items-center px-2 py-1.5">
+                    <Avatar className="w-10 h-10 mr-2">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <p className="text-sm font-semibold">Frk</p>
+                      <p className="text-xs text-muted-foreground">
+                        {userEmail}
+                      </p>
+                    </div>
+                  </div>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel className="text-muted-foreground text-xs font-normal">
+                    Trellix
+                  </DropdownMenuLabel>
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                      Colour scheme
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                          {theme === "dark" ? (
+                            <CheckIcon className="w-3 h-3 mr-1.5" />
+                          ) : null}
+                          Dark mode
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                          {theme === "light" ? (
+                            <CheckIcon className="w-3 h-3 mr-1.5" />
+                          ) : null}
+                          Light mode
+                        </DropdownMenuItem>
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={logout}>Log out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </header>
