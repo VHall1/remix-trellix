39caf482cb9db876e401c2f7a078b0485bfcc9e1
VHall1 <victorhallpsn@hotmail.com>
2024-06-01 20:41:49 +0100

    stub board view

---
+++ app/routes/boards.$boardId/route.tsx
@@ -0,0 +1,11 @@
+import { Shell } from "~/components/shell";
+
+export default function Board() {
+  return (
+    <Shell>
+      <div className="p-2">
+        <div className="flex">test</div>
+      </div>
+    </Shell>
+  );
+}
